b9fcb32d9c0add26229936cdfb136ea0c44521a6
Ekaterina Kostina <ekat.kos@email.cz>
2024-12-26 17:13:09 +0100

    add solution for 04-request-lifecycle task 01-nestjs-components

--- 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
+++ 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
@@ -1,5 +1,29 @@
 import { NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { Observable, tap, map } from "rxjs";
 
 export class ApiVersionInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {}
+	intercept(context: ExecutionContext, next: CallHandler) {
+
+		let runtime: number;
+
+	const start = Date.now();
+
+		return next.handle().pipe(
+			
+		tap(() => {
+			const end = Date.now()
+			runtime =  end - start;
+		
+		}),
+			map((response) => {
+			
+			return {
+				tasks: response.tasks,
+				apiVersion: '1.0',
+				executionTime: `${runtime}ms`,
+			};
+			
+      })
+    );
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
+++ 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
@@ -7,15 +7,20 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto, UpdateTaskDto } from "./task.model";
+import { RolesGuard } from "../guards/roles.guard";
+import { ApiVersionInterceptor } from "../interceptors/api-version.interceptor";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Get()
+	@Get()
+	@UseInterceptors(ApiVersionInterceptor)
   getAllTasks() {
     return this.tasksService.getAllTasks();
   }
@@ -25,7 +30,8 @@ export class TasksController {
     return this.tasksService.getTaskById(id);
   }
 
-  @Post()
+	@Post()
+	@UseGuards(RolesGuard)
   createTask(@Body() task: CreateTaskDto) {
     return this.tasksService.createTask(task);
   }
@@ -38,7 +44,8 @@ export class TasksController {
     return this.tasksService.updateTask(id, task);
   }
 
-  @Delete(":id")
+	@Delete(":id")
+	@UseGuards(RolesGuard)
   deleteTask(@Param("id", ParseIntPipe) id: number) {
     return this.tasksService.deleteTask(id);
   }
